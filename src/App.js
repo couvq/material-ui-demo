e070a13a4324b5b7bc7dfab9b87f5fe9c4eb8ad5
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-09 11:57:13 -0500

    practice with material ui (stopped at ~28:54)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button
+} from "@material-ui/core";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 
 const App = () => {
-    return (
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
         <div>
-            <h1>Hello, World!</h1>
+          <Container maxWidth="sm" style={{ marginTop: '100px' }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone this is a photo album and I'm trying to make this
+              sentence as long as possible so we can see how does it look like
+              on the screen
+            </Typography>
+            <div>
+                <Grid container spacing={2} justify="center">
+                    <Grid item>
+                        <Button variant="contained" color="primary">
+                            See my photos
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="outlined" color="primary">
+                            Secondary Action
+                        </Button>
+                    </Grid>
+                </Grid>
+            </div>
+          </Container>
         </div>
-    );
-}
+      </main>
+    </>
+  );
+};
 
 export default App;
